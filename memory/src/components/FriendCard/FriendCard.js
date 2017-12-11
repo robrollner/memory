a129bcad36566363339b12a1a60044e9fff26e21
Robert Rollner <robrollner@gmail.com>
2017-12-10 18:33:28 -0700

    updated coloring and style

--- memory/src/components/FriendCard/FriendCard.js
+++ memory/src/components/FriendCard/FriendCard.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./FriendCard.css";
 
 const FriendCard = props => (
-  <div className="card">
+  <div className="card z-depth-5 card-image">
     <div className="img-container" dataid={props.id} onClick={() => props.randomRender(props.id)}>
       <img alt={props.name} src={props.image}/>
     </div>   
--- memory/src/components/ScoreCard/ScoreCard.js
+++ memory/src/components/ScoreCard/ScoreCard.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./ScoreCard.css";
 
 const ScoreCard = props => (
-    <div className="score">
+    <div className="score right-align">
         <h6>Score: {props.score}  High Score: {props.hiScore}</h6>
     </div>
 );
